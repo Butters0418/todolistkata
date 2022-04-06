cdf9f56e87c816e0092e7d5dd43da17dd8919367
Butters <swki418@gmail.com>
2022-04-06 17:42:33 +0800

    local api ok

---
+++ errorHandler.js
@@ -0,0 +1,17 @@
+function errorHandler(res){
+  const headers = {
+    'Access-Control-Allow-Headers': 'Content-Type, Authorization, Content-Length, X-Requested-With',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'PATCH, POST, GET,OPTIONS,DELETE',
+    'Content-Type': 'application/json'
+  }
+
+  res.writeHead(404,headers);
+  res.write(JSON.stringify({
+      "status": "false",
+      "message": "無此網站路由1"
+  }));
+  res.end();
+}
+
+module.exports = errorHandler;
